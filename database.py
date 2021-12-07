63ec6f791cb9b6d94e34e0344884868820e39c51
Godiez1 <Godjangg@gmail.com>
2021-12-07 21:26:08 +0700

    OoOoOoOoOoOoooO

--- database.py
+++ database.py
@@ -1,26 +1,23 @@
 import csv
 
 
-class savedata:
+class Db:
     def __init__(self) -> None:
-        """Initilize with all current history(database)"""
-        def __init__(self) -> None:
-            self.database = []
-            with open('winner.csv', 'r') as data:
-                now_data = csv.DictReader(data)
-                for each_data in now_data:
-                    self.database.append(each_data)
+        self.database = []
+        with open('kongkang.csv', 'r') as data:
+            now_data = csv.DictReader(data)
+            for each_data in now_data:
+                self.database.append(each_data)
 
-    def update_data(self, mode, player1, winner, player2=''):
-        '''Update player data in each round'''
+    def update_data(self, mode, player1, player2, winner):
         new_data = {'mode': mode, 'player1': player1,
                     'player2': player2, 'winner': winner}
         self.database.append(new_data)
         self.update_file()
 
     def update_file(self):
-        '''Update a data into CSV file'''
-        with open('winner.csv', 'w') as data:
+
+        with open('kongkang.csv', 'w') as data:
             writer = csv.DictWriter(
                 data, fieldnames=['mode', 'player1', 'player2', 'winner'])
 
